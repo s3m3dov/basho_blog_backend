9d1bf06116ed635a998039a0148dca59add52b36
Hikmat Samadov <s3m3dov@hotmail.com>
2022-04-11 22:09:56 +0400

    refactor: enhance error_handling

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -9,8 +9,8 @@ class UserController extends BaseController
         $requestMethod = $_SERVER["REQUEST_METHOD"];
         $arrQueryStringParams = $this->getQueryStringParams();
 
-        $strErrorMessage = '';
-        $strErrorHeader = '';
+        $errorMessage = '';
+        $errorCode = '';
         $responseData = null;
 
         if ($requestMethod == 'GET') {
@@ -24,25 +24,25 @@ class UserController extends BaseController
                     $offset = $this->getOffset($arrQueryStringParams);
                     $rawData = $userModel->getAllUsers(limit:$limit, offset:$offset);
                 }
-                $responseData = json_encode($rawData);
+                $responseData = empty($rawData) ? null : json_encode($rawData);
             } catch (Exception $e) {
-                $strErrorMessage = $e->getMessage().' Something went wrong! Please contact support.';
-                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+                $errorMessage = $e->getMessage().' Something went wrong! Please contact support.';
+                $errorCode = '500 Internal Server Error';
             }
         } else {
-            $strErrorMessage = 'Method not supported';
-            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+            $errorMessage = 'Method not supported';
+            $errorCode = '422 Unprocessable Entity';
         }
 
         // send output
-        if (!$strErrorMessage) {
+        if ($errorMessage != '') {
+            $this->sendError($errorCode, $errorMessage);
+        } elseif (!$responseData) {
+            $this->sendError('404 Not Found', 'No data found');
+        } else {
             $this->sendOutput(
                 $responseData,
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
-            );
-        } else {
-            $this->sendOutput(json_encode(array('message' => $strErrorMessage)),
-                array('Content-Type: application/json', $strErrorHeader)
             );
         }
     }
