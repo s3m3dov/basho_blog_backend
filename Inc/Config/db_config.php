365a61fd5e21e7dc104ea88eeeb8222132b9a353
Hikmat Samadov <s3m3dov@hotmail.com>
2022-04-14 02:29:59 +0400

    chore: add php-jwt source to project

--- Inc/Config/db_config.php
+++ Inc/Config/db_config.php
@@ -1,4 +1,6 @@
 <?php
+
+// DB Constant Configurations
 const DB_HOST = "localhost";
 const DB_PORT = 3306;
 const DB_USERNAME = "api_user";
--- Inc/bootstrap.php
+++ Inc/bootstrap.php
@@ -2,7 +2,7 @@
 const PROJECT_ROOT_PATH = __DIR__ . "/../";
 
 // include main configuration file
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
+require_once PROJECT_ROOT_PATH . "/inc/db_config.php";
 
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
